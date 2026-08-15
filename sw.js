692df377edaae36eaf8404b3c5466dbac5da31cc
priya19692001-glitch <priya19692001@gmail.com>
2026-08-15 09:18:42 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daily-logbook-v6";
+const CACHE = "daily-logbook-v8";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
